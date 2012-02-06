8c7657bebad5ed089458bee1b39d4e0fccfbf6dc
Reece Hart <reecehart@gmail.com>
2012-02-05 19:25:05 -0800

    adapted all Gene methods to "product gene commentary" (which require refseq accession)

--- lib/python/locus/ncbi/__init__.py
+++ lib/python/locus/ncbi/__init__.py
@@ -7,3 +7,6 @@
 
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
+
+
+__all__ = ['gene','refseq']
--- lib/python/locus/ncbi/gene.py
+++ lib/python/locus/ncbi/gene.py
@@ -17,41 +17,42 @@ class Gene(object):
     def summary(self):
         return self._root.xpath('/Entrezgene-Set/Entrezgene/Entrezgene_summary/text()')[0]
 
-    def grch37p5_exons(self,acv):
-        gc = self._grch37p5_product_commentary(acv)
-        assert len(gc.findall('Gene-commentary_products/Gene-commentary')) == 1, 'Found more than one gene product commentary'
-        i = gc.iterfind('Gene-commentary_genomic-coords/Seq-loc/Seq-loc_mix/Seq-loc-mix/Seq-loc/Seq-loc_int')
-        return [ (int(n[0].find('Seq-interval_from').text),int(n[0].find('Seq-interval_to').text)) for n in i ]
+    def grch37p5_product_exons(self,acv):
+        gc = self._grch37p5_product_gc(acv)
+        i = gc.iterfind('Gene-commentary_genomic-coords/Seq-loc/Seq-loc_mix/Seq-loc-mix/Seq-loc/Seq-loc_int/Seq-interval')
+        return [ (int(n.find('Seq-interval_from').text),int(n.find('Seq-interval_to').text)) for n in i ]
 
-    def grch37p5_exon_starts_ends_lengths(self):
-        exons = self.grch37p5_exons()
+    def grch37p5_product_exon_starts_ends_lengths(self,acv):
+        exons = self.grch37p5_product_exons(acv)
         starts = [ e[0] for e in exons ]
         ends = [ e[1] for e in exons ]
         lengths = [ e[1]-e[0]+1 for e in exons ]
         return starts,ends,lengths
 
-    def grch37p5_strand(self):
-        gc = self._grch37p5_gene_commentary()
-        return gc.find('Gene-commentary_seqs/Seq-loc/Seq-loc_int/Seq-interval/Seq-interval_strand/Na-strand').get('value')
+    def grch37p5_product_strand(self,acv):
+        gc = self._grch37p5_product_gc(acv)
+        n = gc.find('Gene-commentary_genomic-coords/Seq-loc/Seq-loc_mix/Seq-loc-mix/Seq-loc/Seq-loc_int/Seq-interval')
+        return n.find('Seq-interval_strand/Na-strand').get('value')
 
     ######################################################################
     ## Internal functions
 
-    def _grch37p5_product_commentary(self,acv):
+    def _grch37p5_product_gc(self,acv):
         ac,v = acv.split('.')
-        gc = _grch37p5_gene_commentary()
-        pred = ' and '.join(['Gene-commentary_heading[text()="{heading}"]',
-                             'Gene-commentary_accession[text()="{ac}"]',
-                             'Gene-commentary_version[text()="{v}"'])
-        pred = pred.format(heading=heading, ac=ac, v=v)
-        xpath = 'Gene-commentary-products/Gene-commentary[%s]' % (pred)
-        return gc.xpath(xpath)
+        gc = self._grch37p5_gene_commentary()
+        pred = ' and '.join(['Gene-commentary_accession/text()="{ac}"',
+                             'Gene-commentary_version/text()="{v}"'])
+        pred = pred.format(ac=ac, v=v)
+        xpath = 'Gene-commentary_products/Gene-commentary[%s]' % (pred)
+        nodes = gc.xpath(xpath)
+        assert len(nodes) == 1, "Expected only one matching Gene-commentary_products for %s"%(acv)
+        return nodes[0]
 
     def _grch37p5_gene_commentary(self):
-        return self._gene_commentary(self,heading='Reference GRCh37.p5 Primary Assembly')
+        return self._gene_commentary(heading='Reference GRCh37.p5 Primary Assembly')
 
     def _gene_commentary(self,heading):
-        xpath = '/Entrezgene-Set/Entrezgene/Entrezgene_locus/Gene-commentary[%s]' % (pred)
+        xpath = '/Entrezgene-Set/Entrezgene/Entrezgene_locus/Gene-commentary[Gene-commentary_heading[text()="%s"]]' % (heading)
         return self._root.xpath(xpath)[0]
 
 
